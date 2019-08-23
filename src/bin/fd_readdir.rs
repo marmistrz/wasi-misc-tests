3fe08a0e6e789ed34ce99f313fda15478853f71d
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2019-08-23 09:27:44 +0200

    Add a check for ino.

--- src/bin/fd_readdir.rs
+++ src/bin/fd_readdir.rs
@@ -1,7 +1,7 @@
 use libc;
 use misc_tests::open_scratch_directory;
-use misc_tests::wasi::wasi_fd_readdir;
-use std::{env, mem, process, str, slice, cmp::min};
+use misc_tests::wasi::{wasi_fd_filestat_get, wasi_fd_readdir};
+use std::{cmp::min, env, mem, process, slice, str};
 
 const BUF_LEN: usize = 256;
 
@@ -16,9 +16,7 @@ struct ReadDir<'a> {
 
 impl<'a> ReadDir<'a> {
     fn from_slice(buf: &'a [u8]) -> Self {
-        Self {
-            buf,
-        }
+        Self { buf }
     }
 }
 
@@ -50,26 +48,47 @@ impl<'a> Iterator for ReadDir<'a> {
 }
 
 fn test_fd_readdir(dir_fd: libc::__wasi_fd_t) {
+    let mut stat: libc::__wasi_filestat_t = unsafe { mem::zeroed() };
+    let status = wasi_fd_filestat_get(dir_fd, &mut stat);
+    assert_eq!(
+        status,
+        libc::__WASI_ESUCCESS,
+        "reading scratch directory stats"
+    );
+
     let mut buf: [u8; BUF_LEN] = [0; BUF_LEN];
     let mut bufused = unsafe { mem::zeroed() };
     let status = wasi_fd_readdir(dir_fd, &mut buf, BUF_LEN, 0, &mut bufused);
     assert_eq!(status, libc::__WASI_ESUCCESS, "fd_readdir");
     // Create a file in the scratch directory.
 
-    let sl = unsafe { slice::from_raw_parts(buf.as_ptr(), min(BUF_LEN, bufused)) } ;
+    let sl = unsafe { slice::from_raw_parts(buf.as_ptr(), min(BUF_LEN, bufused)) };
     let mut dirs = ReadDir::from_slice(sl);
 
     // the first entry should be `.`
     let dir = dirs.next().unwrap();
     assert_eq!(dir.name, ".", "first name");
-    assert_eq!(dir.dirent.d_type, libc::__WASI_FILETYPE_REGULAR_FILE, "first type"); // WHY??
+    assert_eq!(
+        dir.dirent.d_type,
+        libc::__WASI_FILETYPE_REGULAR_FILE,
+        "first type"
+    ); // WHY??
+    assert_eq!(dir.dirent.d_ino, stat.st_ino);
+    assert_eq!(dir.dirent.d_namlen, 1);
 
     // the second entry should be `..`
     let dir = dirs.next().unwrap();
     assert_eq!(dir.name, "..", "second name");
-    assert_eq!(dir.dirent.d_type, libc::__WASI_FILETYPE_REGULAR_FILE, "second type"); // WHY??
+    assert_eq!(
+        dir.dirent.d_type,
+        libc::__WASI_FILETYPE_REGULAR_FILE,
+        "second type"
+    ); // WHY??
 
-    assert!(dirs.next().is_none(), "the directory should be seen as empty");
+    assert!(
+        dirs.next().is_none(),
+        "the directory should be seen as empty"
+    );
 }
 fn main() {
     let mut args = env::args();
